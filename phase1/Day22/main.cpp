50073222811fbd3fdabe093b7e5ac0dc23c3a844
mrminhajkk <minhaj19mr2001@gmail.com>
2024-12-05 19:13:07 +0530

    day 22

---
+++ phase1/Day22/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "user_mgr.h"
+using namespace std;
+
+string username = "Sooraj";
+int main() {
+    int id = 100;
+    display_username();
+    cout << "id: " << id << endl;
+    return 0;
+}
+
---
+++ phase1/Day22/user_mgr.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "user_mgr.h"
+using namespace std;
+
+extern string username;
+void display_username() {
+    cout << "Username: " << username  << endl;
+}
